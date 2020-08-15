7540c5086faf3b334dfed259f6957c618b5af436
Soter Ramirez <soterrc@gmail.com>
2020-08-14 21:03:18 -0500

    Conexión a MongoAtlas una instancia de MongoDB

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    cors: process.env.CORS,
+    db_user: process.env.DB_USER,
+    db_pass: process.env.DB_PASS,
+    db_host: process.env.DB_HOST,
+    db_name: process.env.DB_NAME
 };
 
 module.exports = { config };
